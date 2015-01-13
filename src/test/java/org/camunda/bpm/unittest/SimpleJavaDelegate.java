b478319efa65f2f05d3537ef1f576a47d82cc904
Galen A Hollins <Galen.A.Hollins@jpl.nasa.gov>
2015-01-13 15:48:03 -0800

    updates

--- src/test/java/org/camunda/bpm/unittest/SimpleJavaDelegate.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleJavaDelegate.java
@@ -3,10 +3,10 @@ package org.camunda.bpm.unittest;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class SleepDelegate implements JavaDelegate {
+public class SimpleJavaDelegate implements JavaDelegate {
 	
 	public void execute(DelegateExecution execution) throws Exception {
-		System.out.println(Thread.currentThread() + " -- LockingDelegate is running");
+		System.out.println(Thread.currentThread() + " -- SimpleJavaDelegate is running");
 		execution.setVariable(Thread.currentThread().toString(), Thread.currentThread().toString());
 	}
 
